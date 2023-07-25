1dcfede9b0f8c1d25111df8011e31137034fc12a
shamim1845 <samimraj1845@gmail.com>
2023-07-25 19:47:22 +0600

    print_pc page modified

--- app/print_pc/page.tsx
+++ app/print_pc/page.tsx
@@ -5,8 +5,7 @@ const page = () => {
 
   return (
     <div className="h-screen lg:h-[93vh] lg:overflow-y-scroll  px-2 lg:px-5">
-      {/* <PDFGenerator /> */}
-      <h1 className="text-2xl"> print pc</h1>
+      <PDFGenerator />
     </div>
   );
 };
